c5344639694ab17650e8faf9cd2a44f69d9678ba
rasheed <radeyola@gmail.com>
2024-01-04 07:52:38 -0800

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic operations imported from calculator_1"""
+    """Handle basic operations from calculator file."""
     from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-mycalculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operators = {"+": add, "-": sub, "*": mul, "/": div}

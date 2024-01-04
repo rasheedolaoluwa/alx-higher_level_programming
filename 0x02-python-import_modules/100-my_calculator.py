39c73de1c1dcf83093f70a674cf5a6a341a9c7e9
rasheed <radeyola@gmail.com>
2024-01-04 14:13:30 -0800

    python is cool

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     operators = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(operators.keys()):
-        print("Unknown operator. Available operators: +, -, * and /\n")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])

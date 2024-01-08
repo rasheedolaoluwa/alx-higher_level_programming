fd60303732cd4c8832cdc3f4e8a6b26ce30d9ce6
rasheed <radeyola@gmail.com>
2024-01-08 02:51:33 -0800

    indent errpr

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,4 +10,4 @@ def print_matrix_integer(matrix = [[]]):
                 else:
                     endspace = ''
                 print("{:d}".format(matrix[row][item], end = endspace)
-            print()
+        print()

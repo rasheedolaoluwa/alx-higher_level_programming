cb933072f1a18d449a49e2f39e304f241714db1d
rasheed <radeyola@gmail.com>
2024-01-22 15:09:44 -0800

    safe print int

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Prints a value as an integer and handles exceptions.
+
+    Args:
+        value (int): The value to be printed as an integer.
+
+    Returns:
+        bool: True if the value is printed as an integer, False if a TypeError 
+              or ValueError occurs. In case of an exception, the error message 
+              is printed to standard error.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+

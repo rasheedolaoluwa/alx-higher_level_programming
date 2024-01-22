ecfb7cbb9d2c492ed38bef82f16796daa4f3f1e5
rasheed <radeyola@gmail.com>
2024-01-22 15:07:37 -0800

    raise exception msg

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError exception with a specified message.
+
+    Args:
+        message (str): The message to be included with the NameError exception.
+    """
+    raise NameError(message)

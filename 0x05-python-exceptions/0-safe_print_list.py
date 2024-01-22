46038e80a5a1b7d67c4dc116a0125c05cebdf9fb
rasheed <radeyola@gmail.com>
2024-01-22 14:40:01 -0800

    betty

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         int: The actual number of elements successfully printed.
     """
-    
+
     printed_count = 0
 
     for i in range(x):

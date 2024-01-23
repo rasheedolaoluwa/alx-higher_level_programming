61221e485acdb3caa653964834a100de93ac87b9
rasheed <radeyola@gmail.com>
2024-01-23 00:30:16 -0800

    square 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+This module defines the class Square.
+"""
+
+class Square:
+    """
+    Represents a square geometric shape.
+
+    Attributes:
+        __size (int): The length of the square's sides.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with a specified size.
+
+        Args:
+            size (int, optional): The length of the sides of the square.
+                                   Defaults to 0.
+
+        Raises:
+            TypeError: If 'size' is not an integer.
+            ValueError: If 'size' is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size

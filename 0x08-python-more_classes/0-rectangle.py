85600b12d12d4f2d7da183cad4a7e602eb65ba6e
rasheed <radeyola@gmail.com>
2024-01-29 08:37:30 -0800

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    This class is currently a placeholder for future developments,
+    with no functionality implemented yet.
+    """
+    pass

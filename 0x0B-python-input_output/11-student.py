a80aa298f93a0a4d742b85dd3bb271698c38fb8e
rasheed <radeyola@gmail.com>
2024-02-06 11:47:19 -0800

    update student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance.
+
+        Sets the student's first name, last name, and age.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the Student instance.
+
+        Filters the dictionary to only include specified attributes if `attrs`
+        is a list of strings.
+
+        Args:
+            attrs (list): An optional list of attribute names to include
+                          in the resulting dictionary.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for key in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Updates the student's attributes from a JSON dictionary.
+
+        Args:
+            json (dict): A dictionary of attributes to update the student's
+                         attributes with.
+        """
+        for k, value in json.items():
+            setattr(self, k, value)

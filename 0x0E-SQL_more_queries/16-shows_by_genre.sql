aa455599fab7ae9516bb0773e8b823a558b52ae2
rasheed <radeyola@gmail.com>
2024-03-13 15:05:25 -0700

    batch 2

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
